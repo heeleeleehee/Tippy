422a2f049bd1f6dfda77d3f510cccc86fb0c3171
Lily He <lilyhe253@gmail.com>
2020-01-14 15:18:43 -0800

    Initial Commit

---
+++ Tippy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tippy
+//
+//  Created by Lily He on 1/14/20.
+//  Copyright © 2020 Lily He. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
